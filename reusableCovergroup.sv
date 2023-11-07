8d39c5ecfbd6049ab5888faad5162e748604fd03
Amol Salunke <149754016+salunkead@users.noreply.github.com>
2023-11-07 12:27:46 +0530

    concept of reusable covergroup

--- reusableCovergroup.sv
+++ reusableCovergroup.sv
@@ -1,4 +1,5 @@
 //reusable covergroup
+//Example-1
 module t;
   bit [3:0]a,b;
   covergroup cg(ref bit [3:0]temp);
@@ -27,3 +28,38 @@ module t;
     end
   
 endmodule
+
+//Example-2
+module test;
+  bit [3:0]a;
+  bit [3:0]b;
+  int max,min;
+  covergroup cg(ref bit[3:0]temp,int max,int min);
+    option.per_instance=1;
+    coverpoint temp{
+      bins used[]={[min:max]};
+    }
+  endgroup
+  initial
+    begin
+      cg c,c1;
+      max=10;min=5;
+      c=new(a,max,min);
+      repeat(20)
+        begin
+          a=$urandom();
+          c.sample();
+          #10;
+        end
+      max=10;min=1;
+      c1=new(b,max,min);
+      repeat(20)
+        begin
+          b=$urandom();
+          c1.sample();
+          #10;
+        end
+    end
+      
+endmodule
+
