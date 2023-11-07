e1a8d24fdba1d8211494d6889be086d5e41201f5
Amol Salunke <149754016+salunkead@users.noreply.github.com>
2023-11-07 14:33:10 +0530

    How to handle x,z,? in coverage.

--- xz?InCoverage.sv
+++ xz?InCoverage.sv
@@ -39,3 +39,60 @@ module test;
       $display("a=%0b",a);
     end
 endmodule
+
+//
+//how to handle xxx(unknown) and zzz(high impedance)
+module test;
+  reg [2:0]state;
+  bit clk;
+  always #5 clk=~clk;
+  covergroup cg@(posedge clk);
+    option.per_instance=1;
+    coverpoint state{
+      bins default_st1={3'bxxx};
+      bins default_st2[]=state with (item>4);
+      bins valid_st[]=state with (item<4);
+    }
+  endgroup
+  initial
+    begin                                                                                                                                               
+      cg c=new;
+      repeat(30)
+        begin
+          @(posedge clk);
+          state=$urandom;
+        end
+    end
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+      #300 $finish;
+    end
+endmodule
+
+////////
+module test;
+  reg [2:0]state;
+  bit clk;
+  always #5 clk=~clk;
+  covergroup cg@(posedge clk);
+    option.per_instance=1;
+    coverpoint state{
+      bins unknown={3'bxxx};
+      bins high_impedance={3'bzzz};
+    }
+  endgroup
+  initial
+    begin                                                                                                                                               
+      cg c=new;
+      @(posedge clk);
+      state=3'bzzz;
+    end
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+      #20 $finish;
+    end
+endmodule
