9dfdbf5b7dfcdf53489e4997ee30f98934cdb4e1
Amol Salunke <salunkeamol10@gmail.com>
2024-02-05 15:56:51 +0530

    Update specifyBins.sv

--- specifyBins.sv
+++ specifyBins.sv
@@ -1,4 +1,3 @@
-//different ways of specifying bins
 module test;
   reg [3:0]a;
   covergroup cg;
@@ -7,9 +6,9 @@ module test;
       bins used[]={[5:$]}; // 5 to last individual bins
       bins used1={[5:$]};  //single bin for 5 to 15 if any one hits coverage for that bin will be 100%
       bins used2={0};      //look for only zero value 
-      bins used3[]={0,8,9,10} // 4 bins of 0,8,9,10
-      bins used4[]={[2:4]} //3 bins 2,3,4
-      bins used[2]={[0:3]}; //4/2=2 2 bins created 1st bin will cover 0,1 and second bin will cover 2,3
+      bins used3[]={0,8,9,10}; // 4 bins of 0,8,9,10
+      bins used4[]={[2:4]}; //3 bins 2,3,4
+      bins used5[2]={[0:3]}; //4/2=2 2 bins created 1st bin will cover 0,1 and second bin will cover 2,3
 }
   endgroup
   initial
