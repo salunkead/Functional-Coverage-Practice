665b9cf0599331855eaa5b8a7a0b6716bcbd5593
Amol Salunke <salunkeamol10@gmail.com>
2024-02-05 17:27:29 +0530

    how to handle x,z in coverage

--- xz?InCoverage.sv
+++ xz?InCoverage.sv
@@ -1,10 +1,9 @@
-//how to handle x,z and ? in coverage
+//how to handle x,z in coverage
 module test;
   reg [3:0]a;
   covergroup cg;
     coverpoint a{
       bins def[]={[0:15]};
-      bins undef={4'b10?0,4'b?101};//single bin covers 1000,1010,0101,1101
       bins unk={4'b10xx,4'bxx10};//single bin covers 10xx and xx10
       bins imp={4'b10zz,4'bzz10};//single bin covers 10zz and zz10
     }
@@ -16,13 +15,13 @@ module test;
       a=4'b1010;
       c.sample();
       $display("a=%0b",a);
-      a=4'b10?0;
+      a=4'b1000;
       c.sample();
       $display("a=%0b",a);
       a=4'b0101;
       c.sample();
       $display("a=%0b",a);
-      a=4'b?101;
+      a=4'b1101;
       c.sample();
       $display("a=%0b",a);
       a=4'b10xx;
