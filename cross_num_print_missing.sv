b711f7201b9af2ac68b0383002f8582ff0d6a97c
Amol Salunke <salunkeamol10@gmail.com>
2024-03-07 11:46:11 +0530

    The option - cross_num_print_missing

--- cross_num_print_missing.sv
+++ cross_num_print_missing.sv
@@ -1,13 +1,17 @@
 //The option - cross_num_print_missing
 /*
 1.The option cross_num_print_missing gives detail about missing cross combination
+2.syntax:- option.cross_num_print_missing=number;
+3.By default,simulator doesnot show missing cross bins by using this option we can get missing cross bins
 */
+
+/////////////
 module test;
   bit [3:0]num;
   bit wr;
   covergroup cg@(num or wr);
     option.per_instance=1;
-    option.cross_num_print_missing=1;
+    option.cross_num_print_missing=10;  //if there are 10 missing cross bins then it will show
     num1:coverpoint num{
       bins valid[]={[0:10]};
     }
@@ -20,7 +24,7 @@ module test;
   initial
     begin
       cg c=new;
-      repeat(100)
+      repeat(10)
         begin
           num=$urandom;
           wr=$urandom;
